8714e1a04d90515505084537f3ed10b7ea14e6e5
Smokyy14 <fdsmdfr985@gmail.com>
2025-01-10 13:45:31 -0300

    Update join.js

--- src/commands/Grupos/join.js
+++ src/commands/Grupos/join.js
@@ -8,24 +8,24 @@ module.exports = {
     
     execute: async (sock, msg, args) => {
       const groupLink = args[0]; 
-  
-        const remoteJid = msg.messages[0].key.remoteJid;
+      const info = msg.messages[0];
+      const remoteJid = info.key.remoteJid;
   
         if (remoteJid.endsWith('@g.us')) {
               return sock.sendMessage(remoteJid, { text: "Este comando no funciona en grupos." });
           }
         
       if (!groupLink) {
-        return sock.sendMessage(msg.messages[0].key.remoteJid, {
+        return sock.sendMessage(remoteJid, {
           text: "Por favor, proporciona un enlace valido.\n_Uso correcto:_ -join [enlace]",
         });
       }
-  
-      // Extraer la parte clave del enlace para unirse al grupo (la parte después de "chat.whatsapp.com/")
+
+      //  Extrae la parte importante del enlace.
       const inviteCode = groupLink.split('chat.whatsapp.com/')[1];
   
       if (!inviteCode) {
-        return sock.sendMessage(msg.messages[0].key.remoteJid, {
+        return sock.sendMessage(remoteJid, {
           text: "El enlace proporcionado no es válido.",
         });
       }
@@ -34,18 +34,18 @@ module.exports = {
         const response = await sock.groupAcceptInvite(inviteCode);
         
         if (response) {
-          sock.sendMessage(msg.messages[0].key.remoteJid, {
+          sock.sendMessage(remoteJid, {
             text: "Me he unido exitosamente a tu grupo.",
           });
         } else {
-          sock.sendMessage(msg.messages[0].key.remoteJid, {
+          sock.sendMessage(remoteJid, {
             text: "He enviado solicitud para que me unas a tu grupo.",
           });
         }
   
       } catch (error) {
         console.error(error);
-        sock.sendMessage(msg.messages[0].key.remoteJid, {
+        sock.sendMessage(remoteJid, {
           text: "Ocurrió un error al intentar unirme al grupo.\nIntenta de nuevo más tarde.",
         });
       }
