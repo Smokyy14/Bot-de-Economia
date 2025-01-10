5a209beaf3a51126066165f5c8ae835d8bf09cb4
Smokyy14 <fdsmdfr985@gmail.com>
2025-01-10 13:39:59 -0300

    Update crime.js

--- src/commands/Economia/crime.js
+++ src/commands/Economia/crime.js
@@ -6,6 +6,7 @@ module.exports = {
   use: "-crime",
   description: "Comete un crimen para obtener monedas.",
   category: "💰 Economia",
+  subcategory: 'Economia',
   grupo: true,
   economy: true,
 
